d7d2f3a0d619454f6fbfdc9c4aebeaba13a85d07
Adam Green <adam.green@adgefficiency.com>
2024-06-28 18:31:45 +1200

    feat: nb

--- fine-tune.py
+++ fine-tune.py
@@ -18,9 +18,9 @@ import shutil
 
 import pathlib
 
+drive.mount("/content/drive")
 output_dir = pathlib.Path("./drive/MyDrive/fine-tune-codellama/")
 output_dir.mkdir(exist_ok=True)
-drive.mount("/content/drive")
 
 wandb.login()
 run = wandb.init(
@@ -122,7 +122,9 @@ max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
 print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
 print(f"{start_gpu_memory} GB of memory reserved.")
 
-trainer_stats = trainer.train()
+trainer_stats = trainer.train(
+    resume_from_checkpoint=output_dir / "checkpoint-200",
+)
 
 used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
 used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
