9b97b8b81a8e3eeaa8b362738e1912d2274425df
Adam Green <adam.green@adgefficiency.com>
2024-06-29 20:14:31 +1200

    2024-06-28 (#1)
    First experiment with a local coding assistant.

---
+++ dataset.py
@@ -0,0 +1,113 @@
+import collections
+import datasets
+
+import pathlib
+
+data = [
+    {
+        "instruction": "Complete the code snippet",
+        "input": '''class BatteryConfig(pydantic.BaseModel):
+            """Battery asset configuration."""
+
+            name: str
+            charge_power_mw: float
+            discharge_power_mw: float
+            capacity_mwh: float
+            efficiency_pct: float
+            initial_charge_mwh: float = 0.0
+            final_charge_mwh: float | None = 0.0
+            freq_mins: int
+            ''',
+        "output": '''@pydantic.field_validator("name")
+            @classmethod
+            def check_name(cls, name: str) -> str:
+                """Ensure we can identify this asset correctly.
+
+                Args:
+                    name: asset name.
+
+                Returns:
+                    The asset name.
+                """
+                assert "battery" in name
+                return name
+        ''',
+    }
+]
+
+base = pathlib.Path.home() / "energy-py-linear"
+
+fis = []
+for fi in base.rglob("*.py"):
+    fis.append(fi)
+
+tr_fis = []
+te_fis = []
+for fi in fis:
+    for pattern in ["evs", "chp"]:
+        if pattern in str(fi):
+            te_fis.append(fi)
+        else:
+            tr_fis.append(fi)
+
+
+def extract_prompts(fis):
+    data = []
+    for fi in fis:
+        code = fi.read_text()
+        chunk_size = 200
+
+        for i in range(0, len(code), chunk_size):
+            inp = code[i : i + chunk_size]
+            out = code[i + chunk_size : i + 2 * chunk_size]
+
+            if len(out) > 12:
+                data.append(
+                    {
+                        "instruction": "Complete the code snippet",
+                        "input": inp,
+                        "output": out,
+                    }
+                )
+                data.append(
+                    {
+                        "instruction": "Guess the file name based on the following code snippet.",
+                        "input": inp,
+                        "output": str(fi.relative_to(pathlib.Path.home())),
+                    }
+                )
+
+    ds = collections.defaultdict(list)
+    for d in data:
+        ds["instruction"].append(d["instruction"])
+        ds["input"].append(d["input"])
+        ds["output"].append(d["output"])
+        ds["prompt"].append(
+            f"Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction: {d['instruction']}\n\n### Input: {d['input']}"
+        )
+        ds["prompt-response"].append(
+            f"{ds['prompt'][-1]}\n\n### Response: {d['output']}"
+        )
+
+    return ds
+
+
+tr_ds = extract_prompts(tr_fis)
+te_ds = extract_prompts(te_fis)
+
+"""
+other tasks
+
+generating tests
+- could just grab part of the battery file, and ask it to predict part of the tests for battery?
+
+TODO
+- only including files from certain modules
+"""
+
+print(f"{len(tr_ds['prompt'])} training examples.")
+print(f"{len(te_ds['prompt'])} test examples.")
+train = datasets.Dataset.from_dict(tr_ds)
+test = datasets.Dataset.from_dict(te_ds)
+ds = datasets.DatasetDict({"train": train, "test": test})
+ds.push_to_hub("adgefficiency/energy-py-linear")
---
+++ fine-tune.py
@@ -0,0 +1,177 @@
+# Original notebook - https://colab.research.google.com/drive/135ced7oHytdxu3N2DNe1Z0kqjyYIkDXp?usp=sharing
+# Installs Unsloth, Xformers (Flash Attention) and all other packages!
+
+# !pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+# !pip install --no-deps xformers "trl<0.9.0" peft accelerate bitsandbytes
+# !pip install wandb -U
+
+# +
+from unsloth import FastLanguageModel
+import torch
+import datasets
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from unsloth import is_bfloat16_supported
+from google.colab import drive
+import wandb
+import shutil
+
+import pathlib
+
+
+def format_prompt(examples: dict):
+    prompt_responses = []
+    for example in examples["prompt-response"]:
+        prompt_responses.append(
+            example + tokenizer.eos_token,
+        )
+    return {"prompt-responses": prompt_responses}
+
+
+def latest_checkpoint(output_dir: pathlib.Path):
+    """Finds the latest checkpoints if any exist."""
+    checkpoints = [
+        cp for cp in output_dir.iterdir() if cp.is_dir() and "checkpoint-" in cp.name
+    ]
+    if checkpoints:
+        latest_checkpoint = max(checkpoints, key=lambda cp: int(cp.name.split("-")[-1]))
+        return str(latest_checkpoint)
+    return None
+
+
+drive.mount("/content/drive")
+output_dir = pathlib.Path("./drive/MyDrive/fine-tune-codellama/")
+output_dir.mkdir(exist_ok=True)
+wandb.login()
+run = wandb.init(project="Fine tuning Codellama")
+
+evaluate_after_training = False
+save_after_training = True
+
+# -
+# # Model and Tokenizer
+
+# +
+max_seq_length = 2048
+dtype = None
+load_in_4bit = True
+model = "unsloth/codellama-7b-bnb-4bit"
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name=model,
+    max_seq_length=max_seq_length,
+    dtype=dtype,
+    load_in_4bit=load_in_4bit,
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+    ],
+    lora_alpha=16,
+    lora_dropout=0,
+    bias="none",
+    use_gradient_checkpointing="unsloth",
+    random_state=3407,
+    use_rslora=False,
+    loftq_config=None,
+)
+# -
+# # Load Dataset
+
+# +
+dataset = datasets.load_dataset("adgefficiency/energy-py-linear", split="train")
+dataset = dataset.map(format_prompt, batched=True)
+dataset_te = datasets.load_dataset("adgefficiency/energy-py-linear", split="test")
+dataset_te = dataset_te.map(format_prompt, batched=True)
+# -
+# # Training
+
+# +
+trainer = SFTTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    train_dataset=dataset,
+    eval_dataset=dataset_te,
+    dataset_text_field="prompt-response",
+    max_seq_length=max_seq_length,
+    dataset_num_proc=2,
+    packing=False,  # Can make training 5x faster for short sequences.
+    args=TrainingArguments(
+        bf16=is_bfloat16_supported(),
+        eval_steps=250,
+        save_steps=100,
+        eval_strategy="steps",
+        fp16=not is_bfloat16_supported(),
+        gradient_accumulation_steps=4,
+        learning_rate=2e-4,
+        logging_steps=10,
+        lr_scheduler_type="linear",
+        num_train_epochs=2,
+        optim="adamw_8bit",
+        output_dir=output_dir,
+        per_device_train_batch_size=8,
+        report_to="wandb",
+        seed=3407,
+        warmup_steps=5,
+        weight_decay=0.01,
+        load_best_model_at_end=True,
+    ),
+)
+
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+trainer_stats = trainer.train(resume_from_checkpoint=latest_checkpoint(output_dir))
+
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory / max_memory * 100, 3)
+lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(
+    f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
+)
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+# -
+# # Inference
+
+# +
+FastLanguageModel.for_inference(model)  # Enable native 2x faster inference
+inputs = tokenizer([dataset["prompt"][0]], return_tensors="pt").to("cuda")
+outputs = model.generate(**inputs, max_new_tokens=64, use_cache=True)
+decoded_outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+for response in decoded_outputs:
+    print(f"{response=}")
+    print(f"{dataset['output'][0]=}")
+# -
+# # Evaluate
+
+# +
+if evaluate_after_training:
+    eval_results = trainer.evaluate()
+    print(f"Evaluation results: {eval_results}")
+# -
+# # GGUF / llama.cpp Conversion
+
+# +
+if save_after_training:
+    model.save_pretrained_gguf("model", tokenizer, quantization_method="q5_k_m")
+    source_path = "model/unsloth.Q5_K_M.gguf"
+    destination_path = output_dir / source_path
+    pathlib.Path(output_dir / "model").mkdir(exist_ok=True)
+    shutil.copy(source_path, destination_path)
